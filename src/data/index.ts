cbf67f63473102dd83a807665e6aaa6c0596c87e
Robin Malfait <malfait.robin@gmail.com>
2023-07-24 23:58:39 +0200

    make TypeScript happy
    Let's define the contract that we expect.

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,2 +1,7 @@
-export {}
+import { Account } from '~/domain/account/account'
+import { Invoice } from '~/domain/invoice/invoice'
+
+export let me: Account
+export let invoices: Invoice[]
+
 module.exports = require(`./${process.env.DATA_SOURCE_FILE}.ts`)
