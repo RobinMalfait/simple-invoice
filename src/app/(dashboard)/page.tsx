acb80e1e77f54b371dcb379f5a1c71bf85077d6b
Robin Malfait <malfait.robin@gmail.com>
2023-08-06 19:24:01 +0200

    add dummy placeholder dashboard page

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="isolate mx-auto w-full max-w-7xl space-y-8 px-4 py-8 sm:px-6 lg:px-8">
+      <div>Dashboard</div>
+    </main>
+  )
+}
