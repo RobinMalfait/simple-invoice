aea819e2444bc3cb96366a1b6e01b0add2b7ef55
Robin Malfait <malfait.robin@gmail.com>
2023-08-06 14:35:29 +0200

    show layered tiny invoices
    If you have 2-3 "invoices" (quote | invoice | receipt) that are related,
    then we will show them as a single layered entity.

--- src/ui/invoice/tiny-invoice.tsx
+++ src/ui/invoice/tiny-invoice.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { CalendarIcon } from '@heroicons/react/24/outline'
+import { CalendarIcon, RectangleStackIcon } from '@heroicons/react/24/outline'
 import { format } from 'date-fns'
 
 import { Invoice } from '~/domain/invoice/invoice'
@@ -17,11 +17,27 @@ type Entity = Quote | Invoice | Receipt
 
 export function TinyInvoice({ invoice }: { invoice: Entity }) {
   let t = useTranslation()
+  let isLayered = match(
+    invoice.type,
+    {
+      quote: () => false,
+      invoice: (e: Invoice) => e.quote !== null,
+      receipt: () => true,
+    },
+    invoice,
+  )
 
   return (
-    <div className="overflow-hidden rounded-md bg-white shadow dark:bg-zinc-950">
-      <div className="flex aspect-a4 w-full shrink-0 flex-col bg-gradient-to-br from-rose-50/90 to-blue-50/90 dark:from-rose-200/90 dark:to-blue-200/90">
-        <div className="flex items-center justify-between bg-gray-50 p-3 text-gray-500 dark:bg-zinc-950/75 dark:text-gray-300">
+    <div className="group relative rounded-md bg-white shadow transition-transform duration-300 will-change-transform hover:-translate-y-1 dark:bg-zinc-950">
+      {isLayered && (
+        <>
+          <div className="absolute inset-0 -z-10 h-full w-full rotate-2 rounded-md bg-gray-100 ring-1 ring-black/5 drop-shadow transition-[transform,opacity] duration-200 will-change-[transform,opacity] group-hover:rotate-0 group-hover:opacity-0 dark:bg-zinc-700"></div>
+          <div className="absolute inset-0 -z-10 h-full w-full -rotate-1 rounded-md bg-gray-50 ring-1 ring-black/5 drop-shadow transition-transform duration-200 will-change-[transform,opacity] group-hover:rotate-0 group-hover:opacity-0 dark:bg-zinc-600"></div>
+        </>
+      )}
+
+      <div className="relative z-10 flex aspect-a4 w-full shrink-0 flex-col rounded-md bg-gradient-to-br from-rose-50/90 to-blue-50/90 ring-1 ring-black/5 dark:from-rose-200/90 dark:to-blue-200/90">
+        <div className="flex items-center justify-between rounded-t-md bg-gray-50 p-3 text-gray-500 dark:bg-zinc-950/75 dark:text-gray-300">
           {invoice.client.name}
           {match(
             invoice.type,
@@ -36,7 +52,14 @@ export function TinyInvoice({ invoice }: { invoice: Entity }) {
           )}
         </div>
 
-        <div className="flex flex-1 items-center justify-center border-y border-black/5">
+        <div className="relative flex flex-1 items-center justify-center border-y border-black/5">
+          {isLayered && (
+            <div className="absolute bottom-2 right-2 flex">
+              <div className="rounded-md bg-black/5 p-2">
+                <RectangleStackIcon className="h-5 w-5" />
+              </div>
+            </div>
+          )}
           <div className="text-center">
             {match(invoice.type, {
               quote: () => <small className="lowercase">{t((x) => x.quote.title)}</small>,
@@ -52,7 +75,7 @@ export function TinyInvoice({ invoice }: { invoice: Entity }) {
           </div>
         </div>
 
-        <div className="flex items-center justify-between bg-gray-50 p-3 dark:bg-zinc-950/75">
+        <div className="flex items-center justify-between rounded-b-md bg-gray-50 p-3 dark:bg-zinc-950/75">
           <span className="flex items-center gap-1 text-xs text-gray-500 dark:text-gray-300">
             <CalendarIcon className="h-4 w-4 text-gray-400 dark:text-white" />
             <span>
