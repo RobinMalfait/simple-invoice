2cdb362dd0e7b4c67612129baf40dcd8d315163e
Robin Malfait <malfait.robin@gmail.com>
2023-08-07 19:47:42 +0200

    make navbar slightly seethrough + blur

--- src/ui/navbar.tsx
+++ src/ui/navbar.tsx
@@ -18,15 +18,18 @@ export function Navbar() {
   }
 
   return (
-    <Disclosure as="nav" className="sticky top-0 z-10 bg-gray-800 dark:bg-zinc-950 print:hidden">
+    <Disclosure
+      as="nav"
+      className="sticky top-0 z-10 bg-gray-800/90 shadow backdrop-blur dark:bg-zinc-950/95 print:hidden"
+    >
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
-              <XMarkIcon className="ui-open:block ui-not-open:hidden h-6 w-6" aria-hidden="true" />
-              <Bars3Icon className="ui-open:hidden ui-not-open:block h-6 w-6" aria-hidden="true" />
+              <XMarkIcon className="h-6 w-6 ui-open:block ui-not-open:hidden" aria-hidden="true" />
+              <Bars3Icon className="h-6 w-6 ui-open:hidden ui-not-open:block" aria-hidden="true" />
             </Disclosure.Button>
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
