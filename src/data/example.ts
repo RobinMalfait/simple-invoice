d86ea22440f25688d4c07248274a9b12a76bea3b
Robin Malfait <malfait.robin@gmail.com>
2023-08-11 00:22:25 +0200

    add pdf configuration to example data

--- src/data/example.ts
+++ src/data/example.ts
@@ -59,6 +59,36 @@ configure({
      * calculate the next invoice number.
      */
     numberStrategy: new IncrementStrategy().next,
+
+    /**
+     * The configuration for PDF generation.
+     */
+    pdf: {
+      /**
+       *  The filename template for the PDF. You can use every property of the invoice as a placeholder.
+       *  - For example `{{number}}` will be replaced by the invoice number.
+       *  - You can also use nested properties, for example `{{client.name}}` will be replaced by the
+       *    client name.
+       *  - For dates, you can use a format string, for example `{{issueDate:dd-MM-yyyy}}`
+       */
+      filename: 'invoice-{{number}}.pdf',
+    },
+  },
+
+  receipt: {
+    /**
+     * The configuration for PDF generation.
+     */
+    pdf: {
+      /**
+       *  The filename template for the PDF. You can use every property of the receipt as a placeholder.
+       *  - For example `{{number}}` will be replaced by the receipt number.
+       *  - You can also use nested properties, for example `{{client.name}}` will be replaced by the
+       *    client name.
+       *  - For dates, you can use a format string, for example `{{receiptDate:dd-MM-yyyy}}`
+       */
+      filename: 'receipt-{{number}}.pdf',
+    },
   },
 })
 
