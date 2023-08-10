40e4e518e5101763fee99abbfd466e842971e23c
Robin Malfait <malfait.robin@gmail.com>
2023-08-10 20:38:16 +0200

    sort dashboard invoices similar to `/invoices`

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,9 +1,16 @@
 import { Dashboard } from './dashboard'
 
+import { compareDesc } from 'date-fns'
 import { me, invoices as rawInvoices } from '~/data'
+import { resolveRelevantEntityDate } from '~/domain/relevant-entity-date'
 import { squashEntities } from '~/domain/squash-entities'
 
 export default function Page() {
-  let invoices = squashEntities(rawInvoices).sort((a, z) => z.number.localeCompare(a.number))
+  let invoices = squashEntities(rawInvoices).sort(
+    (a, z) =>
+      compareDesc(resolveRelevantEntityDate(a), resolveRelevantEntityDate(z)) ||
+      z.number.localeCompare(a.number),
+  )
+
   return <Dashboard me={me} invoices={invoices} />
 }
