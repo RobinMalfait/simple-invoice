227e3f0497566d5a2578733d3d1411ed362e709e
Robin Malfait <malfait.robin@gmail.com>
2023-09-03 22:36:32 +0200

    render all money values as 10k when classified
    This ensures that the classified box will always be very similar and you
    can't guess if a certain value is a lot or not at all.

--- src/ui/money.tsx
+++ src/ui/money.tsx
@@ -1,14 +1,15 @@
 'use client'
 
-import { Classified } from '~/ui/classified'
+import { Classified, useIsClassified } from '~/ui/classified'
 import { useCurrencyFormatter } from '~/ui/hooks/use-currency-formatter'
 
 export function Money({ amount, short = false }: { amount: number; short?: boolean }) {
   let currencyFormatter = useCurrencyFormatter({ type: short ? 'short' : 'long' })
+  let isClassified = useIsClassified()
 
   return (
     <span className="font-sans tabular-nums">
-      <Classified>{currencyFormatter.format(amount / 100)}</Classified>
+      <Classified>{currencyFormatter.format(isClassified ? 10000 : amount / 100)}</Classified>
     </span>
   )
 }
