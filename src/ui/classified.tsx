23e124d805cf316a277ae794dc2656aeced3679c
Robin Malfait <malfait.robin@gmail.com>
2023-09-04 00:36:12 +0200

    improve Classified mode
    Do not rely on computed information. Let's just use CSS...

--- src/ui/classified.tsx
+++ src/ui/classified.tsx
@@ -1,8 +1,7 @@
 'use client'
 
-import { createContext, Fragment, useContext, useEffect, useRef } from 'react'
+import { createContext, Fragment, useContext, useEffect } from 'react'
 import { classNames } from '~/ui/class-names'
-import { useClientRect } from '~/ui/hooks/use-client-rect'
 
 let ClassifiedContext = createContext(true)
 
@@ -26,26 +25,17 @@ export function useIsClassified() {
 export function Classified(props: { children: React.ReactNode }) {
   let classified = useIsClassified()
 
-  let container = useRef<HTMLDivElement | null>(null)
-  let { width = 0, height = 0 } = useClientRect(container)
-
   return (
     <>
       <span
-        ref={container}
         className={classNames(
-          classified && 'pointer-events-none relative inline-block select-none',
+          classified &&
+            'pointer-events-none relative -mx-2 -my-1 inline-block select-none px-2 py-1',
         )}
       >
         <Fragment>{props.children}</Fragment>
         {classified && (
-          <span
-            style={{
-              width: width + 8,
-              height: height + 4,
-            }}
-            className="pointer-events-none absolute left-1/2 top-1/2 z-50 h-full w-full -translate-x-1/2 -translate-y-1/2 select-none bg-zinc-950"
-          />
+          <span className="pointer-events-none absolute inset-0 z-50 h-full w-full select-none bg-zinc-950" />
         )}
       </span>
     </>
