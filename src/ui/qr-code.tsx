de1e96c638e1eeaa6ff55709db98703e43760e72
Robin Malfait <malfait.robin@gmail.com>
2023-10-09 21:54:25 +0200

    allow to pass `margin` and `scale` to `<QRCode />`

--- src/ui/qr-code.tsx
+++ src/ui/qr-code.tsx
@@ -1,7 +1,15 @@
 import { toDataURL } from 'qrcode'
 
-export async function QRCode({ children }: { children: string }) {
-  let qrCode = await toDataURL(children, { margin: 0 })
+export async function QRCode({
+  children,
+  margin = 0,
+  scale = 4,
+}: {
+  children: string
+  margin?: number
+  scale?: number
+}) {
+  let qrCode = await toDataURL(children, { margin, scale })
 
   // eslint-disable-next-line @next/next/no-img-element
   return <img alt="" src={qrCode} />
