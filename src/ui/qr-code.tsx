e09abfdb17bd30d56d53b2f78a8d29794a9c1dec
Robin Malfait <malfait.robin@gmail.com>
2023-10-09 23:04:31 +0200

    lazy load QR code

--- src/ui/qr-code.tsx
+++ src/ui/qr-code.tsx
@@ -1,6 +1,7 @@
 import { toDataURL } from 'qrcode'
+import { useEffect, useState } from 'react'
 
-export async function QRCode({
+export function QRCode({
   children,
   margin = 0,
   scale = 4,
@@ -9,8 +10,22 @@ export async function QRCode({
   margin?: number
   scale?: number
 }) {
-  let qrCode = await toDataURL(children, { margin, scale })
+  let [qrCode, setQrCode] = useState<string | null>(null)
+  useEffect(() => {
+    let state = {
+      mounted: true,
+    }
+
+    toDataURL(children, { margin, scale }).then((data) => {
+      if (!state.mounted) return
+      setQrCode(data)
+    })
+
+    return () => {
+      state.mounted = false
+    }
+  }, [children, margin, scale])
 
   // eslint-disable-next-line @next/next/no-img-element
-  return <img alt="" src={qrCode} />
+  return qrCode === null ? null : <img alt="" src={qrCode} />
 }
