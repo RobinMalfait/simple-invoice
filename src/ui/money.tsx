5ca17adb061d55724d9dee2a146db1d8d2c33a5f
Robin Malfait <malfait.robin@gmail.com>
2023-11-18 21:42:22 +0100

    passthrough props to `span` in `Money` component

--- src/ui/money.tsx
+++ src/ui/money.tsx
@@ -2,13 +2,19 @@
 
 import { Classified, useIsClassified } from '~/ui/classified'
 import { useCurrencyFormatter } from '~/ui/hooks/use-currency-formatter'
+import { classNames } from './class-names'
 
-export function Money({ amount, short = false }: { amount: number; short?: boolean }) {
+export function Money({
+  amount,
+  short = false,
+  className,
+  ...props
+}: { amount: number; short?: boolean } & React.ComponentProps<'span'>) {
   let currencyFormatter = useCurrencyFormatter({ type: short ? 'short' : 'long' })
   let isClassified = useIsClassified()
 
   return (
-    <span className="font-sans tabular-nums">
+    <span className={classNames(className, 'font-sans tabular-nums')} {...props}>
       <Classified>{currencyFormatter.format(isClassified ? 10000 : amount / 100)}</Classified>
     </span>
   )
