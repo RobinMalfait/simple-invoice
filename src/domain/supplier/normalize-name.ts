e3aa4e715d981ce35834a3b2e91198c02e86fe82
Robin Malfait <malfait.robin@gmail.com>
2024-03-03 23:49:19 +0100

    improve `normalizeName`
    - Start using title case from 3 characters
    - Add PREFERS_UPPERCASE, PREFERS_LOWERCASE and PREFERS_TITLE_CASE for a
      set of words that typically have a known case

--- src/domain/supplier/normalize-name.ts
+++ src/domain/supplier/normalize-name.ts
@@ -8,13 +8,21 @@ export function normalizeName(name: string) {
     .replace(/[*,_]/g, ' ')
     .replace(/\s+/g, ' ') // Remove duplicate spaces
     // Uppercase first letter of each word
-    .replace(/([-\s0-9])*([a-z])([^\s-]{4,})/gi, (_, prefix = '', firstLetter, rest) => {
+    .replace(/([-\s0-9])*([a-z])([^\s-]{3,})/gi, (_, prefix = '', firstLetter, rest) => {
       return prefix + firstLetter.toUpperCase() + rest.toLowerCase()
     })
     // Replace short abbreviations like `ABC-Foo` with `ABC Foo`
     .replace(/([A-Z]{2,3})-/g, '$1 ')
     .replace(/\s+/g, ' ') // Remove duplicate spaces
     .replace(/\(.\)/g, '') // Remove single characters in parentheses
+    .replace(/\b\w*\b/g, (word) => {
+      if (PREFERS_UPPERCASE.has(word.toLowerCase())) return word.toUpperCase()
+      if (PREFERS_LOWERCASE.has(word.toLowerCase())) return word.toLowerCase()
+      if (PREFERS_TITLE_CASE.has(word.toLowerCase())) {
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+      }
+      return word
+    })
     .trim()
 
   // Close non-closed parentheses
@@ -43,3 +51,27 @@ export function normalizeName(name: string) {
     .replace(/\.$/g, '') // Remove trailing dot
     .trim()
 }
+
+let PREFERS_UPPERCASE = new Set<string>([
+  // Company formats, Belgium (dutch)
+  'nv',
+  'bv',
+  'bvba', // Deprecated
+  'cv',
+  'vof',
+  'commv',
+  'vzw',
+  'ivzw',
+  // Company formats, Belgium (french)
+  'sa',
+  'srl',
+  'sc',
+  'snc',
+  'scomm',
+  'asbl',
+  'aisbl',
+  // Company formats, USA
+  'llc',
+])
+let PREFERS_LOWERCASE = new Set<string>(['ikea'])
+let PREFERS_TITLE_CASE = new Set<string>(['the'])
