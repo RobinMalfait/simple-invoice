ba6370cc4fce399ea6688ca12a63aaa6f276d6da
Robin Malfait <malfait.robin@gmail.com>
2024-03-03 17:01:58 +0100

    fix strange import

--- src/ui/hooks/use-currency-formatter.ts
+++ src/ui/hooks/use-currency-formatter.ts
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 
-import { useI18N } from '~/ui/hooks//use-i18n'
+import { useI18N } from '~/ui/hooks/use-i18n'
 import { createCurrencyFormatter } from '~/utils/currency-formatter'
 
 export function useCurrencyFormatter({ type = 'long' }: { type?: 'short' | 'long' } = {}) {
