27fe1bb83053e0f4f5b7597ab0bb344d05d1b98a
Robin Malfait <malfait.robin@gmail.com>
2024-04-01 01:06:14 +0200

    simplify record list

--- src/ui/record/list.tsx
+++ src/ui/record/list.tsx
@@ -68,123 +68,119 @@ export function RecordList({ records }: { records: Record[] }) {
   let currentYear = format(new Date(), 'y')
   let currentQuarter = titleForQuarter(new Date())
 
+  if (records.length <= 0) {
+    return <Empty message="No records yet" />
+  }
+
   return (
     <div>
-      {records.length > 0 ? (
-        <>
-          {groupRecords(records).map(([year, records]) => {
-            return (
-              <Disclosure key={year} defaultOpen={true} as="div">
-                <div className="relative flex w-full flex-col">
-                  <DisclosureButton className="sticky top-4 isolate z-30 px-4">
-                    <div className="absolute -left-4 -right-8 -top-8 bottom-0 bg-gray-100/75 backdrop-blur dark:bg-zinc-800/75"></div>
+      {groupRecords(records).map(([year, records]) => {
+        return (
+          <Disclosure key={year} defaultOpen={true} as="div">
+            <div className="relative flex w-full flex-col">
+              <DisclosureButton className="sticky top-4 isolate z-30 px-4">
+                <div className="absolute -left-4 -right-8 -top-8 bottom-0 bg-gray-100/75 backdrop-blur dark:bg-zinc-800/75"></div>
 
-                    <div className="absolute inset-y-3 left-0 flex h-6 w-6 flex-none -translate-x-12 items-center justify-center bg-gray-100 dark:bg-zinc-800">
-                      <div
-                        className={classNames(
-                          'h-1.5 w-1.5 rounded-full ',
-                          year === currentYear
-                            ? 'bg-blue-400 ring-1 ring-blue-400 ring-offset-4 ring-offset-gray-100 dark:ring-offset-zinc-800'
-                            : 'bg-gray-300 ring-1 ring-gray-300 dark:bg-zinc-500 dark:ring-zinc-500',
-                        )}
-                      />
-                    </div>
+                <div className="absolute inset-y-3 left-0 flex h-6 w-6 flex-none -translate-x-12 items-center justify-center bg-gray-100 dark:bg-zinc-800">
+                  <div
+                    className={classNames(
+                      'h-1.5 w-1.5 rounded-full ',
+                      year === currentYear
+                        ? 'bg-blue-400 ring-1 ring-blue-400 ring-offset-4 ring-offset-gray-100 dark:ring-offset-zinc-800'
+                        : 'bg-gray-300 ring-1 ring-gray-300 dark:bg-zinc-500 dark:ring-zinc-500',
+                    )}
+                  />
+                </div>
 
-                    <div className="relative z-20 flex justify-between rounded-md bg-white/60 px-[18px] py-3 text-gray-500 ring-1 ring-black/5 backdrop-blur dark:bg-zinc-900/95 dark:text-gray-400">
-                      <span>{year}</span>
-                      <TotalsByStatus records={Array.from(records.values()).flat(1)} />
-                    </div>
-                  </DisclosureButton>
+                <div className="relative z-20 flex justify-between rounded-md bg-white/60 px-[18px] py-3 text-gray-500 ring-1 ring-black/5 backdrop-blur dark:bg-zinc-900/95 dark:text-gray-400">
+                  <span>{year}</span>
+                  <TotalsByStatus records={Array.from(records.values()).flat(1)} />
+                </div>
+              </DisclosureButton>
 
-                  <DisclosurePanel className="relative w-full space-y-8 px-4 py-8 sm:px-6 lg:px-8">
-                    {Array.from(records.entries()).map(([title, records]) => {
-                      return (
-                        <Disclosure
-                          key={title}
-                          as="div"
-                          defaultOpen={
-                            !records.every((e) => {
-                              return isFuture(resolveRelevantRecordDate(e))
-                            })
-                          }
-                          className="relative flex gap-x-4"
-                        >
-                          <div className="absolute -bottom-8 -top-8 left-0 z-20 flex w-6 -translate-x-2 justify-center">
-                            <div className="w-px bg-gray-300 dark:bg-zinc-500"></div>
-                          </div>
+              <DisclosurePanel className="relative w-full space-y-8 px-4 py-8 sm:px-6 lg:px-8">
+                {Array.from(records.entries()).map(([title, records]) => {
+                  return (
+                    <Disclosure
+                      key={title}
+                      as="div"
+                      defaultOpen={
+                        !records.every((e) => {
+                          return isFuture(resolveRelevantRecordDate(e))
+                        })
+                      }
+                      className="relative flex gap-x-4"
+                    >
+                      <div className="absolute -bottom-8 -top-8 left-0 z-20 flex w-6 -translate-x-2 justify-center">
+                        <div className="w-px bg-gray-300 dark:bg-zinc-500"></div>
+                      </div>
 
-                          <div className="relative ml-10 flex w-full flex-col gap-4">
-                            <div className="sticky top-20 isolate z-20">
-                              <div className="absolute -left-4 -right-8 -top-8 bottom-0 z-10 bg-gray-100/75 backdrop-blur dark:bg-zinc-800/75"></div>
+                      <div className="relative ml-10 flex w-full flex-col gap-4">
+                        <div className="sticky top-20 isolate z-20">
+                          <div className="absolute -left-4 -right-8 -top-8 bottom-0 z-10 bg-gray-100/75 backdrop-blur dark:bg-zinc-800/75"></div>
 
-                              <DisclosureButton className="w-full">
-                                <div className="absolute inset-y-3 left-0 flex h-6 w-6 flex-none -translate-x-12 items-center justify-center bg-gray-100 dark:bg-zinc-800">
-                                  <div
-                                    className={classNames(
-                                      'h-1.5 w-1.5 rounded-full ',
-                                      title === currentQuarter
-                                        ? 'bg-blue-400 ring-1 ring-blue-400 ring-offset-4 ring-offset-gray-100 dark:ring-offset-zinc-800'
-                                        : 'bg-gray-300 ring-1 ring-gray-300 dark:bg-zinc-500 dark:ring-zinc-500',
-                                    )}
-                                  />
-                                </div>
-
-                                <div className="relative z-20 flex w-full justify-between rounded-md bg-white/95 px-[18px] py-3 text-gray-500 ring-1 ring-black/5 backdrop-blur dark:bg-zinc-900/95 dark:text-gray-400">
-                                  <span>{title}</span>
-                                  <TotalsByStatus records={records} />
-                                </div>
-                              </DisclosureButton>
+                          <DisclosureButton className="w-full">
+                            <div className="absolute inset-y-3 left-0 flex h-6 w-6 flex-none -translate-x-12 items-center justify-center bg-gray-100 dark:bg-zinc-800">
+                              <div
+                                className={classNames(
+                                  'h-1.5 w-1.5 rounded-full ',
+                                  title === currentQuarter
+                                    ? 'bg-blue-400 ring-1 ring-blue-400 ring-offset-4 ring-offset-gray-100 dark:ring-offset-zinc-800'
+                                    : 'bg-gray-300 ring-1 ring-gray-300 dark:bg-zinc-500 dark:ring-zinc-500',
+                                )}
+                              />
                             </div>
 
-                            <DisclosurePanel className="grid grid-cols-[repeat(auto-fill,minmax(275px,1fr))] gap-4">
-                              {records.map((record) => {
-                                return (
-                                  <I18NProvider
-                                    key={record.id}
-                                    value={{
-                                      // Prefer the language of the account when looking at the overview of
-                                      // records.
-                                      language: record.account.language,
+                            <div className="relative z-20 flex w-full justify-between rounded-md bg-white/95 px-[18px] py-3 text-gray-500 ring-1 ring-black/5 backdrop-blur dark:bg-zinc-900/95 dark:text-gray-400">
+                              <span>{title}</span>
+                              <TotalsByStatus records={records} />
+                            </div>
+                          </DisclosureButton>
+                        </div>
 
-                                      // Prefer the currency of the client when looking at the overview of
-                                      // records.
-                                      currency: record.client.currency,
-                                    }}
-                                  >
-                                    <Link href={`/${record.type}/${record.number}`}>
-                                      <TinyRecord record={record} />
-                                    </Link>
-                                  </I18NProvider>
-                                )
-                              })}
-                            </DisclosurePanel>
-                          </div>
-                        </Disclosure>
-                      )
-                    })}
-                  </DisclosurePanel>
-                </div>
-              </Disclosure>
-            )
-          })}
+                        <DisclosurePanel className="grid grid-cols-[repeat(auto-fill,minmax(275px,1fr))] gap-4">
+                          {records.map((record) => {
+                            return (
+                              <I18NProvider
+                                key={record.id}
+                                value={{
+                                  // Prefer the language of the account when looking at the overview of
+                                  // records.
+                                  language: record.account.language,
 
-          <div className="relative ml-8 flex -translate-x-2 gap-x-4">
-            <div className="absolute bottom-8 left-0 top-0 flex w-6 justify-center">
-              <div className="w-px bg-gray-300 dark:bg-zinc-500"></div>
+                                  // Prefer the currency of the client when looking at the overview of
+                                  // records.
+                                  currency: record.client.currency,
+                                }}
+                              >
+                                <Link href={`/${record.type}/${record.number}`}>
+                                  <TinyRecord record={record} />
+                                </Link>
+                              </I18NProvider>
+                            )
+                          })}
+                        </DisclosurePanel>
+                      </div>
+                    </Disclosure>
+                  )
+                })}
+              </DisclosurePanel>
             </div>
+          </Disclosure>
+        )
+      })}
 
-            <div className="relative mt-3 flex h-6 w-6 flex-none items-center justify-center bg-gray-100 dark:bg-zinc-800">
-              <div className="h-1.5 w-1.5 rounded-full bg-gray-300 ring-1 ring-gray-300 dark:bg-zinc-500 dark:ring-zinc-500" />
-            </div>
+      <div className="relative ml-8 flex -translate-x-2 gap-x-4">
+        <div className="absolute bottom-8 left-0 top-0 flex w-6 justify-center">
+          <div className="w-px bg-gray-300 dark:bg-zinc-500"></div>
+        </div>
 
-            <div className="py-3 text-gray-500 dark:text-gray-300">
-              The beginning of your adventure
-            </div>
-          </div>
-        </>
-      ) : (
-        <Empty message="No records yet" />
-      )}
+        <div className="relative mt-3 flex h-6 w-6 flex-none items-center justify-center bg-gray-100 dark:bg-zinc-800">
+          <div className="h-1.5 w-1.5 rounded-full bg-gray-300 ring-1 ring-gray-300 dark:bg-zinc-500 dark:ring-zinc-500" />
+        </div>
+
+        <div className="py-3 text-gray-500 dark:text-gray-300">The beginning of your adventure</div>
+      </div>
     </div>
   )
 }
